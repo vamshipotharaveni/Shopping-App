1db7a34b7b3f879f6fb304383b4beb4585dc80e5
vamshipotharaveni <potharavenivamshi2003@gmail.com~>
2024-11-21 16:27:11 +0530

    shopping

--- src/components/ShoppingComponent.js
+++ src/components/ShoppingComponent.js
@@ -3,11 +3,14 @@ import { useState,useEffect } from "react";
 export default function ShoppingComponent(){
     const [category,setCategory]=useState([])
     const [product,setProduct]=useState([])
+    // const [,se]
+    
+    
     function showcatagory(){
       fetch("https://fakestoreapi.com/products/categories")
       .then(response => response.json())
       .then(data=>{
-        data.unshift("All")
+        data.unshift("all")
         setCategory(data)
       })
 
@@ -28,14 +31,15 @@ export default function ShoppingComponent(){
     }
 
     function catergoryGiving(e){
-      if(e.target.value=="All"){
-        showProducts("https://fakestoreapi.com/products");
+      if(e.target.value=='all'){
+        showProducts('https://fakestoreapi.com/products');
       }else{
-        showProducts(`https://fakestoreapi.com/products/category/${e.target.value.toLowerCase()}`);
+        showProducts(`https://fakestoreapi.com/products/category/${e.target.value}`);
       }
-     
-      
+    }
 
+    function cartbutton(e){
+      alert(e.target.id)
     }
 
     useEffect(()=>{
@@ -55,30 +59,34 @@ export default function ShoppingComponent(){
 
   <div className="d-flex">
   
-  <div className="drop-down">
+  <div className="drop-down col-2">
   <select className="m-3 text-white rounded-lg" onChange={catergoryGiving} style={{ backgroundColor: "rgba(0, 0, 0, 0.7)" }}>{
     category.map(mat=>
-      <option key={mat}>{mat.toUpperCase()}</option>
+      <option key={mat}>{mat}</option>
     )
   }
   </select>
   </div>
 
-  <div className="entire info">
-  <main className="d-flex flex-wrap">
+  <div className="entire info col-18">
+  <main className="d-flex flex-wrap ">
       {
         product.map(prod=>
-        <div className="card w-25 p-3  m-2 " >
+        <div className="card w-25 p-3  m-3 " >
           <img src={prod.image} alt="hello" className="card-img-top p-3 w-29" style={{height:"150px"}} key={prod.image}/>
           <div className="info" style={{height:"210px"}}>
-            <dd>{prod.title}</dd>
+            <dd key={prod.title}>{prod.title}</dd>
             <dt>Price</dt>
-            <dd>{prod.price}</dd>
+            <dd key={prod.price}>{prod.price}</dd>
             {/* <p className="text-justify"><span>Price:</span>{prod.description}</p> */}
             <dt>Rating</dt>
-            <dd className="bi bi-star-fill text-success">{prod.rating.rate}</dd>
+            <dd className="bi bi-star-fill text-success" key={prod.rating.rate}>{prod.rating.rate}</dd>
           </div>
-          <button className="btn btn-danger w-100"> <span className="bi bi-cart4"/> Add to cart</button>
+          <div>
+          <button className="btn btn-danger w-100" key={prod.id} id={prod.id} onClick={cartbutton} > 
+          <span className="bi bi-cart4"/> Add to cart
+          </button>
+        </div>
         </div>
         
          
@@ -87,7 +95,11 @@ export default function ShoppingComponent(){
       }
       </main>
       </div>
+      <div className="col-2">
+  <button className="btn btn-danger"><span className="bi bi-cart4"></span>cart items</button>
 </div>
+</div>
+
 </div>
     );
 }
